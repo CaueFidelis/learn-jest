93c819d85be0551df9ec53efbdf44f3bff714a17
Caue <cauefidelis@live.com>
2022-10-26 19:16:09 -0300

    Added subtract function and tests

--- Modulo 1/introducao/src/lib/calculator/calculator.js
+++ Modulo 1/introducao/src/lib/calculator/calculator.js
@@ -2,9 +2,20 @@ module.exports.sum = (num1, num2) => {
   const int1 = parseInt(num1, 10);
   const int2 = parseInt(num2, 10);
 
-  if (Number.isNaN(int1) || Number.isNaN(int2)) {
+  if (Number.isNaN(parseInt(int1, 10)) || Number.isNaN(parseInt(int2, 10))) {
     throw new Error('Please check your input');
   }
 
   return int1 + int2;
+};
+
+module.exports.subtract = (num1, num2) => {
+  const int1 = parseFloat(num1, 10);
+  const int2 = parseFloat(num2, 10);
+
+  if (Number.isNaN(parseInt(int1, 10)) || Number.isNaN(parseInt(int2, 10))) {
+    throw new Error('Please check your input');
+  }
+
+  return int1 - int2;
 };
--- Modulo 1/introducao/src/lib/calculator/calculator.spec.js
+++ Modulo 1/introducao/src/lib/calculator/calculator.spec.js
@@ -1,27 +1,45 @@
-const { sum } = require('./calculator');
+const { sum, subtract } = require('./calculator');
 
-it('should sum 2 and 2 and the result must be 4', () => {
-  expect(sum(2, 2)).toBe(4);
+describe('Sum', () => {
+  it('should sum 2 and 2 and the result must be 4', () => {
+    expect(sum(2, 2)).toBe(4);
+  });
+
+  it('should sum 2 and 2 even if one of them is a string and result must be 4', () => {
+    expect(sum('2', '2')).toBe(4);
+  });
+
+  it('should thow an erro if what is provided to the method cannot be summed', () => {
+    expect(() => {
+      sum('', '2');
+    }).toThrowError();
+
+    expect(() => {
+      sum([2, 2]);
+    }).toThrowError();
+
+    expect(() => {
+      sum({});
+    }).toThrowError();
+
+    expect(() => {
+      sum();
+    }).toThrowError();
+  });
 });
 
-it('should sum 2 and 2 even if one of them is a string and result must be 4', () => {
-  expect(sum('2', '2')).toBe(4);
-});
+describe('Subtract', () => {
+  it('should subtract 5 and 3 and the result must be 2', () => {
+    expect(subtract(5, 3)).toBe(2);
+  });
 
-it('should thow an erro if what is provided to the method cannot be summed', () => {
-  expect(() => {
-    sum('', '2');
-  }).toThrowError();
+  it('should subtract 5 and 3 even if one of them is a string and result must be 2', () => {
+    expect(subtract('5', '3')).toBe(2);
+  });
 
-  expect(() => {
-    sum([2, 2]);
-  }).toThrowError();
-
-  expect(() => {
-    sum({});
-  }).toThrowError();
-
-  expect(() => {
-    sum();
-  }).toThrowError();
+  it('should throw an error if what is provided to the method cannot be subtracted', () => {
+    expect(() => {
+      subtract('', '2');
+    }).toThrowError();
+  });
 });
